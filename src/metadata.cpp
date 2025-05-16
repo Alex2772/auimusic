cf39577d3c9b4c5a4ecb5b458040746c4092e3f7
alex2772 <alex2772sc@gmail.com>
2025-05-16 04:06:32 +0300

    Windows support

--- src/metadata.cpp
+++ src/metadata.cpp
@@ -2,18 +2,25 @@
 // Created by alex2772 on 5/5/25.
 //
 
-#include <dr_mp3.h>
-#include <id3v2lib.h>
+#include <taglib/fileref.h>
+
 #include "metadata.h"
-#include <AUI/Logging/ALogger.h>
 #include <AUI/Image/AImage.h>
 
 void metadata::populate(Song& song) {
-    auto tag = aui::ptr::make_unique_with_deleter(ID3v2_read_tag(song.location.toStdString().c_str()), ID3v2_Tag_free);
-    if (!tag) {
-        return;
-    }
-    if (auto frame = ID3v2_Tag_get_album_cover_frame(tag.get())) {
-        song.thumbnail = AImage::fromBuffer(AByteBufferView(frame->data->data, frame->data->picture_size));
+    TagLib::FileRef f(song.location.toStdString().c_str());
+    auto picture = f.complexProperties("PICTURE");
+    for (const auto& map : picture) {
+        for (const auto&[k, v] : map) {
+            if (k == "data") {
+                bool ok = false;
+                auto vector = v.toByteVector(&ok);
+                if (!ok) {
+                    break;
+                }
+
+                song.thumbnail = AImage::fromBuffer(AByteBufferView(vector.data(), vector.size()));
+            }
+        }
     }
 }
