df9cb2570bd29f649fb7295827fdb6b73138a9fc
alex2772 <alex2772sc@gmail.com>
2025-06-17 14:48:25 +0300

    upadaet vesiron

--- .github/tidy-summary.py
+++ .github/tidy-summary.py
@@ -15,12 +15,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #  SPDX-License-Identifier: GPL-3.0
-#
-#  SPDX-License-Identifier: MPL-2.0
-#
-#  This Source Code Form is subject to the terms of the Mozilla Public
-#  License, v. 2.0. If a copy of the MPL was not distributed with this
-#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 """
 
